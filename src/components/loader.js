55d6d54535bfbd5b2a496e8101e8c49d308c673e
Abubakar Diallo <dialloabubakar5861@gmail.com>
2024-06-13 10:01:02 +0100

    feat: implemented loader component

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Loader = () => {
-  return <div>Loading...</div>
+  return (
+    <h1 className='fixed top-2/4 left-2/4 transform -translate-x-1/2 -translate-y-1/2'>
+      Loading...
+    </h1>
+  )
 }
 
 export default Loader
